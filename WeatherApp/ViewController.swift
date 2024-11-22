eb475b8463c26d3f354ec497d02a87a11d7f20a3
jeanramalho.dev <jeanramalho.dev@gmail.com>
2024-11-22 11:46:15 -0300

    [feat] - Inicia criação programática com viewCode do projeto

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -8,6 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private lazy var backgroundView: UIImageView = {
+        let imageView = UIImageView(frame: .zero)
+        imageView.image = UIImage(named: "background")
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
