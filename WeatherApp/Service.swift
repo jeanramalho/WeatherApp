11faa9604122d4baadd0c1a9d7252bc47297390e
jeanramalho.dev <jeanramalho.dev@gmail.com>
2024-11-28 10:32:45 -0300

    [feat] - Inicia a implementação do serviço de busca de informações na API

---
+++ WeatherApp/Service.swift
@@ -0,0 +1,12 @@
+//
+//  Service.swift
+//  WeatherApp
+//
+//  Created by Jean Ramalho on 28/11/24.
+//
+
+import Foundation
+
+class Service {
+    let baseURL: String = "https://api.openweathermap.org/data/3.0/onecall"
+}
